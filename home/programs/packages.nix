8efc595f6cff20c06ed4feefc3079c61dcf4a07f
Maicol Battistini <maicolbattistini@live.it>
2025-01-15 16:48:02 +0100

    fix: PHP 8.4 environment with selected extensions and Xdebug configuration

--- home/programs/packages.nix
+++ home/programs/packages.nix
@@ -1,4 +1,23 @@
-{ pkgs, ... }: {
+{ pkgs, ... }: let 
+php84custom = pkgs.php84.buildEnv {
+      extensions = ({enabled, all}:
+        enabled ++ (with all; [
+          bcmath
+          curl
+          gd
+          intl
+          mbstring
+          opcache
+          xdebug
+          xml
+          xsl
+          zip
+        ]));
+      extraConfig = ''
+        xdebug.mode=debug
+      '';
+    };
+in {
   home.packages = with pkgs; [
     ###### System ######
     coreutils
@@ -36,16 +55,10 @@
     corepack_latest
     
     # PHP
-    php84
-    php84Packages.composer
-    php84Extensions.bcmath
-    php84Extensions.curl
-    php84Extensions.gd
-    php84Extensions.intl
-    php84Extensions.mbstring
-    php84Extensions.opcache
-    php84Extensions.xml
-    php84Extensions.zip
+  #   php84.withExtensions ({ enabled, all }:
+  # enabled ++ [ all.imagick ])
+    php84custom.out
+    php84custom.packages.composer
     
     ###### Formatters and linters ######
     alejandra # nix
