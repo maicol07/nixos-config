f1953217328fe9f8d3aaa72e8f9eae5480b4eba0
Maicol Battistini <maicolbattistini@live.it>
2025-01-15 20:46:03 +0100

    fix: Add function to handle fish_focus_in event for async prompt compatibility with zoxide

--- home/shell/fish.nix
+++ home/shell/fish.nix
@@ -48,6 +48,12 @@
         # Disable async prompt
         # set -g async_prompt_enable 0
 
+        # Fix incompatibility with zoxide. Source: https://github.com/acomagu/fish-async-prompt/issues/77#issuecomment-2132709455
+        function fish_focus_in --on-event fish_focus_in
+            __async_prompt_fire
+            # commandline -f paint  # Not sure whether this helps or hurts.
+        end
+
         function fish_right_prompt
             rpoc_time
         end
