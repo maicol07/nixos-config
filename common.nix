f1632b17ea792db406aee367f91df01fb116318c
Maicol Battistini <maicolbattistini@live.it>
2025-08-30 14:55:43 +0200

    feat: Added i18n settings

--- common.nix
+++ common.nix
@@ -2,6 +2,21 @@
   # Common system configuration shared across all hosts
   time.timeZone = "Europe/Rome";
 
+  # Select internationalisation properties.
+  i18n.defaultLocale = "it_IT.UTF-8";
+
+  i18n.extraLocaleSettings = {
+    LC_ADDRESS = "it_IT.UTF-8";
+    LC_IDENTIFICATION = "it_IT.UTF-8";
+    LC_MEASUREMENT = "it_IT.UTF-8";
+    LC_MONETARY = "it_IT.UTF-8";
+    LC_NAME = "it_IT.UTF-8";
+    LC_NUMERIC = "it_IT.UTF-8";
+    LC_PAPER = "it_IT.UTF-8";
+    LC_TELEPHONE = "it_IT.UTF-8";
+    LC_TIME = "it_IT.UTF-8";
+  };
+
   networking.hostName = hostname;
 
   programs.fish.enable = true;
