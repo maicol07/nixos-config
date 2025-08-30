0f7b8f861128ed20676f1eba84343562ac1eea7c
Maicol Battistini <maicolbattistini@live.it>
2025-08-30 15:04:06 +0200

    fix(server.nix): add hardware configuration import and refine initial setup

--- server.nix
+++ server.nix
@@ -1,5 +1,60 @@
-_: {
+{pkgs, hostname}: {
   # Base server hardening/services (non-WSL)
+   
+   imports = [
+    # Include the hardware configuration
+    ./hardware-configuration.nix
+   ];
+
+  # Bootloader.
+  boot.loader.grub.enable = true;
+  boot.loader.grub.device = "/dev/sda";
+  boot.loader.grub.useOSProber = true;
+
+  # Use latest kernel.
+  boot.kernelPackages = pkgs.linuxPackages_latest;
+
+  networking.hostName = hostname;
+  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+
+  # Configure network proxy if necessary
+  # networking.proxy.default = "http://user:password@proxy:port/";
+  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
+
+  # Enable networking
+  networking.networkmanager.enable = true;
+  
+  # Configure keymap in X11
+  services.xserver.xkb = {
+    layout = "it";
+    variant = "";
+  };
+
+  # Configure console keymap
+  console.keyMap = "it2";
+
+  # Enable CUPS to print documents.
+  services.printing.enable = true;
+
+  # Enable sound with pipewire.
+  services.pulseaudio.enable = false;
+  security.rtkit.enable = true;
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+    # If you want to use JACK applications, uncomment this
+    #jack.enable = true;
+
+    # use the example session manager (no others are packaged yet so this is enabled by default,
+    # no need to redefine it in your config for now)
+    #media-session.enable = true;
+  };
+
+  # Enable touchpad support (enabled default in most desktopManager).
+  # services.xserver.libinput.enable = true;
+
 
   # Firewall disabled due to external one
   networking.firewall.enable = false;
