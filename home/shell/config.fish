9889cc588e6ee18dca0d587a9aa73bae3db897c2
Maicol Battistini <maicolbattistini@live.it>
2025-09-25 10:57:07 +0200

    feat: add kubecolor support and kubectl completions to fish configuration

--- home/shell/config.fish
+++ home/shell/config.fish
@@ -48,3 +48,24 @@ end
 # end
 
 set sponge_purge_only_on_exit true
+
+# If kubectl is installed
+if type -q kubectl
+    # This needs to be added before "function ... --wraps kubectl"
+    kubectl completion fish | source
+
+    # adds alias for "kubectl" to "kubecolor" with completions
+    function kubectl --wraps kubectl
+        command kubecolor $argv
+    end
+
+    # adds alias for "k" to "kubecolor" with completions
+    function k --wraps kubectl
+        command kubecolor $argv
+    end
+
+    # reuse "kubectl" completions on "kubecolor"
+    function kubecolor --wraps kubectl
+        command kubecolor $argv
+    end
+end
