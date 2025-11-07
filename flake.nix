4d53a0ebd609c61befd2f29590a55797a991b4d7
Maicol Battistini <maicolbattistini@live.it>
2025-11-07 15:16:06 +0100

    fix: update lanzaboote version to v0.4.3
    See nix-community/lanzaboote#485

--- flake.nix
+++ flake.nix
@@ -16,7 +16,7 @@ inputs = {
   nix-index-database.url = "github:Mic92/nix-index-database";
   nix-index-database.inputs.nixpkgs.follows = "nixpkgs";
   lanzaboote = {
-      url = "github:nix-community/lanzaboote/v0.4.2";
+      url = "github:nix-community/lanzaboote/v0.4.3";
 
       # Optional but recommended to limit the size of your system closure.
       inputs.nixpkgs.follows = "nixpkgs";
