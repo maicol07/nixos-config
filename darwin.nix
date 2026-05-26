dfce5958392c1d5373f931f26db6b5536d36089b
Maicol Battistini <maicolbattistini@live.it>
2026-05-26 20:11:24 +0200

    fix: use mkForce for home directory paths in darwin.nix

--- darwin.nix
+++ darwin.nix
@@ -1,7 +1,12 @@
 { config, pkgs, username, ... }: {
   # Define the user's home directory on macOS
   users.users.${username} = {
-    home = "/Users/${username}";
+    home = pkgs.lib.mkForce "/Users/${username}";
+  };
+
+  # Force Home Manager to use the macOS path, overriding the Linux one in home.nix
+  home-manager.users.${username} = {
+    home.homeDirectory = pkgs.lib.mkForce "/Users/${username}";
   };
 
   # Enable the Nix daemon (mandatory on multi-user/macOS installations)
