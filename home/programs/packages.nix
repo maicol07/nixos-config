1a7ed80030d6b0bffe9f458ac9e3c50c841494ee
Maicol Battistini <maicolbattistini@live.it>
2026-05-27 13:40:31 +0200

    feat(home): add macOS package set and kdoctor

--- home/programs/packages.nix
+++ home/programs/packages.nix
@@ -7,5 +7,6 @@ in
     (import ./packages/common.nix { inherit pkgs; })
     ++ lib.optionals isServer (import ./packages/server.nix { inherit pkgs; })
     ++ lib.optionals isPersonal (import ./packages/personal.nix { inherit pkgs; })
-    ++ lib.optionals isLinuxPersonal (import ./packages/linux.nix { inherit pkgs; });
+    ++ lib.optionals isLinuxPersonal (import ./packages/linux.nix { inherit pkgs; })
+    ++ lib.optionals isDarwin (import ./packages/darwin.nix { inherit pkgs; });
 }
