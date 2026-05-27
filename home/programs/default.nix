ab55d3be590afb1e779b5cace1c2ab0cc94a7e15
Maicol Battistini <maicolbattistini@live.it>
2026-05-27 13:40:34 +0200

    fix(home): dynamic NH_FLAKE path and pkgs argument

--- home/programs/default.nix
+++ home/programs/default.nix
@@ -1,4 +1,4 @@
-{ hostname, username, isServer ? false, ... }: {
+{ hostname, username, pkgs, isServer ? false, ... }: {
   imports =
     [
       ./packages.nix
@@ -16,11 +16,11 @@
     gh.enable = true;
     lazygit.enable = true;
     less.enable = true;
-    nh = {
+      nh = {
       enable = true;
       clean.enable = true;
       clean.extraArgs = "--keep-since 4d --keep 3";
-      flake = "/home/" + username + "/.config/nixos"; # sets NH_OS_FLAKE variable for you
+      flake = (if pkgs.stdenv.isDarwin then "/Users/" else "/home/") + username + "/.config/nixos"; # sets NH_OS_FLAKE variable for you
     };
     vivid = {
       enable = true;
