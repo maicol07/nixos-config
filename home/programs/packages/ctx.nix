a3dd58d37351b609ce20199887dda6f409a224a7
Maicol Battistini <maicolbattistini@live.it>
2026-08-07 15:34:58 +0200

    feat: Upgrade ctx version

--- home/programs/packages/ctx.nix
+++ home/programs/packages/ctx.nix
@@ -2,13 +2,13 @@
 
 pkgs.buildGoModule {
   pname = "ctx";
-  version = "0.1.0";
+  version = "0.1.16";
 
   src = pkgs.fetchFromGitHub {
     owner = "vlebo";
     repo = "ctx";
-    rev = "a6941b6acc0703574e2df99cf06b76e4ebbfca72";
-    hash = "sha256-ekggrJX0DTSlslxSZgX/Jv2b21yy4i5ax/ToVykXuPE=";
+    rev = "a7cd9a2a4b5a5ac2a80243784d17751dfeddec37";
+    hash = "sha256-IRRguXDG18N5k7GnALax4Srar9/x+HZgqH+sBFc4g2w=";
   };
 
   vendorHash = "sha256-BPiT+UL74j31g5ftjVpJt0P4lTfdlPViTloKKjMZvcM=";
